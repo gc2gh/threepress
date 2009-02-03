3b838ebbf2cf90a4e86d58ea947f637f5d35f390
liza31337 <liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3>
2009-02-03 16:14:15 +0000

    End point for Bookworm/Xapian integration; trunk moved to MySQL indexing

--- threepress/epub/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('threepress.epub.views',
-                       url(r'/logos$', 'direct_to_template',
-                           {'template': 'logos.html'}, name='logos'),
-                       )
